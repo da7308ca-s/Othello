432d87ac96fe7facac0de0dda22bdb51484df1f8
David <carpen97@gmail.com>
2020-02-19 14:30:11 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -3,13 +3,4 @@ import subprocess
 import sys
 
 proc = subprocess.Popen("./othello", shell=True,stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-
-def send(msg):
-        proc.stdin.write(str.encode(msg + "\n"))
-        proc.stdin.flush()
-        print(msg)
-
-def read():
-        return proc.stdout.readline().decode()
-
-print(read())
+print("test")
