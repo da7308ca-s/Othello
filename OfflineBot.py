b2ba0910e311119aa8759188e03f644ac0225e52
David <carpen97@gmail.com>
2020-02-19 17:08:55 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from subprocess import Popen, PIPE
+from time import sleep
 
 process = Popen("./othello", stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
@@ -13,11 +14,17 @@ def write(msg):
 
 def main():
         print(read())
+        sleep(0.5)
         write("da7308ca-s")
+        sleep(0.5)
         print(read())
+        sleep(0.5)
         write("w")
+        sleep(0.5)
         print(read())
+        sleep(0.5)
         write("e6")
+        sleep(0.5)
         print(read())
   
 if __name__== "__main__":
