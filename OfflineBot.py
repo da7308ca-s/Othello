789150715327286634b497a567d7de20f1be559a
David <carpen97@gmail.com>
2020-02-19 15:00:42 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -2,7 +2,7 @@
 
 from subprocess import Popen, PIPE
 
-process = Popen("./othello", stdout=PIPE, stderr=PIPE)
+process = Popen("./othello", stdout=PIPE, stderr=PIPE, shell=True)
 stdout, stderr = process.communicate()
 print("this far")
 print(stdout)
