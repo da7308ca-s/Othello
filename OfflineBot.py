1b7fe6b43917f28d36cefe21a036810f77f066f1
David <carpen97@gmail.com>
2020-02-19 16:34:44 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -4,4 +4,19 @@ from subprocess import Popen, PIPE
 
 process = Popen("./othello", stdout=PIPE, stderr=PIPE)
 
-print(process.stdout.readline().decode())
+def read():
+        return process.stdout.readline().decode()
+
+def write(msg):
+        proc.stdin.write(str.encode(msg + "\n"))
+        proc.stdin.flush()
+
+def main():
+        print(read())
+        write("da7308ca-s")
+        print(read())
+  
+if __name__== "__main__":
+        main()
+
+
