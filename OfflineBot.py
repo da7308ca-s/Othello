541449086c4a7cd9ece73c5cf43dbd210d6cd82c
David <carpen97@gmail.com>
2020-02-21 23:24:21 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -49,7 +49,7 @@ def choose_move(position,depth,maximizingPlayer):
 		maxEval = -65
 		for c in position.get_children():
 			c.print_board()
-			c.last_move
+			pruint("last move",c.last_move)
 			evaluation = minimax(c,depth,False,-65,65)
 			if evaluation>maxEval:
 				maxEval = evaluation
