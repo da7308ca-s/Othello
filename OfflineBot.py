d1859ddb65aa9e2bc12e500d20817ae2bcd02794
David <carpen97@gmail.com>
2020-02-21 10:08:31 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from subprocess import Popen, PIPE
+import numpy as np
 
 process = Popen("./othello", stdout=PIPE, stderr=PIPE, stdin=PIPE)
 my_color = "w"
+pos = Position()
 
 def read():
         return process.stdout.readline().decode()
@@ -12,6 +14,26 @@ def write(msg):
         process.stdin.write(str.encode(msg + "\n"))
         process.stdin.flush()
         print(msg)
+
+def text_to_coord(text):
+        s = "abcdefgh"
+        return (s.index(text[0]), int(text[1]) - 1)
+
+class Position:
+        def __init__(self):
+                self.board = np.zeros(8,8)
+                self.board[3][3] = -1
+                self.board[4][4] = -1
+                self.board[3][4] = 1
+                self.board[4][3] = 1
+
+        def print_board(self):
+                print(self.board)
+
+        def place_piece(self,move,player):
+                move = text_to_coord(move)
+                self.board[move[0]][move[1]] = 1 if player == "d" else -1
+                self.print_board()
 
 def main(msg):
         print(msg)
@@ -31,10 +53,12 @@ def main(msg):
         elif msg == "my move\n":
                 move = read()
                 print(move)
+                place_piece(move)
                 main(read())
         elif msg == "your move\n":
                 move = "d6"
                 write(move)
+                place_piece(move)
                 main(read())
         elif msg == "\"The game is finished\" White: \n":
                 print(read())
