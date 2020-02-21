8ec4096b49e991d50f93d7e51d9d6e5b4bf08d8c
David <carpen97@gmail.com>
2020-02-21 09:33:58 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python
 
 from subprocess import Popen, PIPE
-from time import sleep
 
 process = Popen("./othello", stdout=PIPE, stderr=PIPE, stdin=PIPE)
+my_color = "w"
 
 def read():
-        stdout = []
-        while True:
-                line = process.stdout.readline()
-                stdout.append(line)
-                print('The line', line)
-                if line == '' and p.poll() != None:
-                        break
-        return ''.join(stdout)
+        return proc.stdout.readline().decode()
 
 def write(msg):
         process.stdin.write(str.encode(msg + "\n"))
         process.stdin.flush()
         print(msg)
 
-def main():
-        print(read())
-        write("da7308ca-s")
-        print(read())
-        write("w")
-        print(read())
-        write("e6")
-        print(read())
+def main(msg):
+        print(msg)
+        if str(msg) == "hi! I am your othello server.\n":
+                send('da7308ca-s')
+                print(read())
+                print(read())
+                main(read())
+        elif msg == "choose colour, 'd' for dark, 'w' for white.\n":
+                send(my_color)
+                print(read())
+                main(read())
+        elif msg == "The game is finished\n":
+                print(read())
+                print(read())
+                print(read())
+        elif msg == "my move\n":
+                move = read()
+                print(move)
+        elif msg == "your move\n":
+                send("e6")
+                main(read())
+        elif msg == "\"The game is finished\" White: \n":
+                print(read())
+                print(read())
+                print(read())
+        else:
+                print("Unknown response:")
+                print(read())
+                print(read())
+                print(read())
+                print(read())
   
 if __name__== "__main__":
-        main()
+        main(read())
 
 
