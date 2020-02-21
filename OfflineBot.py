61f120655b1dcad256cf18752174bfc151bcbb51
David <carpen97@gmail.com>
2020-02-21 15:30:33 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -17,7 +17,7 @@ def text_to_coord(text):
 
 class Position:
 	def __init__(self,board = None):
-		if not board is None:
+		if board is not None:
 			self.board = board
 		else:
 			self.board = np.zeros((8,8))
@@ -93,7 +93,7 @@ class Position:
 						break
 					r = rr
 					c = cc
-					hasOppositeColor = False;
+					hasOppositeColor = False
 					for i in range(7):
 						if direction == 0: 
 							r-=1
