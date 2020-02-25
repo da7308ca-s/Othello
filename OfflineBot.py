bc1151bda918f729894d9d9ee6f6f898f4f1838c
David <carpen97@gmail.com>
2020-02-25 13:40:57 +0100

    m

--- OfflineBot.py
+++ OfflineBot.py
@@ -79,7 +79,6 @@ def minimax(position,depth,maximizingPlayer,pruning = True,alpha = -65,beta = 65
 			if beta<=alpha and pruning:
 				n_pruned +=1
 				break
-		print("h1")
 		return maxEval, move, n_visited_children, n_pruned
 	else:
 		minEval = 65
@@ -93,7 +92,6 @@ def minimax(position,depth,maximizingPlayer,pruning = True,alpha = -65,beta = 65
 			if beta<=alpha and pruning:
 				n_pruned+=1
 				break
-		print("h2")
 		return minEval, move, n_visited_children, n_pruned
 
 class Position:
@@ -275,7 +273,7 @@ def main(msg):
 		print(read())
 
 process = Popen("./othello", stdout=PIPE, stderr=PIPE, stdin=PIPE)
-my_color = "s"
+my_color = "d"
 depth = 1
 pos = Position()
   
