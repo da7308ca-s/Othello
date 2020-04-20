3445d8de8a8722b425c355c59b692b0da6bcab57
David <carpen97@gmail.com>
2020-04-20 15:48:00 +0200

    m

--- othelloPlayer.py
+++ othelloPlayer.py
@@ -249,6 +249,7 @@ class Position:
 		return np.sum(self.board)
 
 def main(msg,d=0):
+	global move_counter
 	if d>5:
 		print("Error")
 		return
@@ -276,7 +277,6 @@ def main(msg,d=0):
 		move = read()
 		print(move)
 		pos.place_piece(text_to_coord(move))
-		global move_counter
 		move_counter+=1
 		pos.print_board()
 		if verbose: 
@@ -304,7 +304,6 @@ def main(msg,d=0):
 		logs[4][game_number].append(pos.evaluate_position())
 		write(coord_to_text(move))
 		pos.place_piece(move)
-		global move_counter
 		move_counter+=1
 		pos.print_board()
 		if verbose: 
